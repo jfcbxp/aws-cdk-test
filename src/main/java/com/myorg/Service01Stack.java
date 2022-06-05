fa2b4b0366fcbf7c58c9d4c079b6127506b60d8a
Joao Felipe <jfcbxp@gmail.com>
2022-06-05 14:03:09 -0300

    adicionado Load Balance Fargate

---
+++ src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,49 @@
+package com.myorg;
+
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class Service01Stack extends Stack {
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01")
+                .serviceName("service-01")
+                .cluster(cluster)
+                .cpu(512)
+                .desiredCount(2)
+                .listenerPort(8080)
+                .memoryLimitMiB(1024)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_project01")
+                                .image(ContainerImage.fromRegistry("jfcbxp/crud_aws_project01:0.0.1-SNAPSHOT"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
+                                                .logGroupName("Service01")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service01")
+                                        .build()))
+                                .build())
+                .publicLoadBalancer(true)
+                .assignPublicIp(true) //necessario pois o natgatways foi definido como 0 para redução da fatura do aws
+                .build();
+    }
+}
