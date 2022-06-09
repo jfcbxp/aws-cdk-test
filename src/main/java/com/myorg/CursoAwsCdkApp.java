76bdd8d053504d993f68f7f4570f24b920d885e5
Joao Felipe <jfcbxp@gmail.com>
2022-06-09 08:16:44 -0300

    adicionado SnsStack a vpc

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -11,8 +11,10 @@ public class CursoAwsCdkApp {
         ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
         clusterStack.addDependency(vpcStack);
 
-        RdsStack rdsStack = new RdsStack(app,"Rds",vpcStack.getVpc());
+        RdsStack rdsStack = new RdsStack(app, "Rds", vpcStack.getVpc());
         rdsStack.addDependency(vpcStack);
+
+        SnsStack snsStack = new SnsStack(app, "Sns");
 
         Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
         service01Stack.addDependency(clusterStack);
