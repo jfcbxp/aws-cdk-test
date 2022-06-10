4297336c3aeb9f48e20de64d2401947484779c6e
Joao Felipe <jfcbxp@gmail.com>
2022-06-10 07:44:33 -0300

    classe Service02Stack

---
+++ src/main/java/com/myorg/Service02Stack.java
@@ -0,0 +1,78 @@
+package com.myorg;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.CpuUtilizationScalingProps;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.ScalableTaskCount;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Service02Stack extends Stack {
+    public Service02Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public Service02Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+
+        Map<String, String> envVariables = new HashMap<>();
+        envVariables.put("AWS_REGION", "us-east-1");
+
+        ApplicationLoadBalancedFargateService service02 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB02")
+                .serviceName("service-02")
+                .cluster(cluster)
+                .cpu(512)
+                .desiredCount(2)
+                .listenerPort(9090)
+                .memoryLimitMiB(1024)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_project02")
+                                .image(ContainerImage.fromRegistry("jfcbxp/crud_aws_project02:0.0.3-SNAPSHOT"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "Service02LogGroup")
+                                                .logGroupName("Service02")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service02")
+                                        .build()))
+                                .environment(envVariables)
+                                .build())
+                .publicLoadBalancer(true)
+                .assignPublicIp(true) //necessario pois o natgatways foi definido como 0 para redução da fatura do aws
+                .build();
+
+        service02.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+                .path("/actuator/health")
+                .port("9090")
+                .healthyHttpCodes("200")
+                .build());
+
+        ScalableTaskCount scalableTaskCount = service02.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .maxCapacity(2)
+                .maxCapacity(4)
+                .build());
+
+        scalableTaskCount.scaleOnCpuUtilization("Service02AutoScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(50)
+                .scaleInCooldown(Duration.seconds(60))
+                .scaleOutCooldown(Duration.seconds(60))
+                .build());
+
+    }
+}
