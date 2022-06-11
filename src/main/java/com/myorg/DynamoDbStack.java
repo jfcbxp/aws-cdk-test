cad3b20f7e9e7e6bd6ce879815c928c3b8330a8e
Joao Felipe <jfcbxp@gmail.com>
2022-06-11 20:28:09 -0300

    adicionado configuracao de autoscaling no DynamoDbStack

--- src/main/java/com/myorg/DynamoDbStack.java
+++ src/main/java/com/myorg/DynamoDbStack.java
@@ -1,12 +1,15 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.EnableScalingProps;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.UtilizationScalingProps;
 import software.constructs.Construct;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
@@ -22,7 +25,7 @@ public class DynamoDbStack extends Stack {
     public DynamoDbStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        productEventsDb = Table.Builder.create(this,"ProductEventsDb")
+        productEventsDb = Table.Builder.create(this, "ProductEventsDb")
                 .tableName("product-events")
                 .billingMode(BillingMode.PROVISIONED) //limita requisoes para otimizar cobranças do aws
                 .readCapacity(1)
@@ -38,6 +41,26 @@ public class DynamoDbStack extends Stack {
                 .timeToLiveAttribute("ttl")
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
+
+        productEventsDb.autoScaleReadCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
+        productEventsDb.autoScaleWriteCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
 
     }
 
